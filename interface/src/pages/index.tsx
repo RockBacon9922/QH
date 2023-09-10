e1965250c771847649c1b64cb09b8e950696cb59
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-09-10 11:20:21 +0000

    moved to t3 as work comptuer couldn't open astro app

---
+++ interface/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>QH Email Creator</title>
+        <meta name="description" content="QH Email Creator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col"></main>
+    </>
+  );
+}
