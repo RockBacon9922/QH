3899cf757b06fea419475449a21abb1006db1551
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-09-12 14:08:20 +0000

    Sidebar UI now works

--- interface/src/pages/index.tsx
+++ interface/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import qhLogo from "../../public/qh logo.png";
-import React, { useState } from "react";
+import React, { type ReactNode, useState } from "react";
+import { object } from "zod";
+import { StoreApi, UseBoundStore, create } from "zustand";
+import { v4 } from "uuid";
 
 type Show = {
+  uuid: string;
   date: string;
   title: string;
   description: string;
@@ -15,14 +19,41 @@ type Email = {
   shows?: Show[];
 };
 
-export default function Home() {
-  const email: Email = {
-    subject: "Queens Hall Presents:",
-  };
-  const [emailJSON, setEmailJson] = useState(email);
-  const setEmailJSON = (newEmailJSON: Email) => {
-    setEmailJson(newEmailJSON);
-  };
+type Action = {
+  addShow: (show: Show) => void;
+  updateShow: (show: Show) => void;
+  removeShow: (show: Show) => void;
+  updateSubject: (subject: string) => void;
+};
+
+const useStore = create<Email & Action>((set) => ({
+  subject: "",
+  addShow: (show) =>
+    set((state) => ({
+      ...state,
+      shows: [...(state.shows ?? []), show],
+    })),
+  updateShow: (show) =>
+    set((state) => ({
+      ...state,
+      shows: state.shows?.map((s) => (s.uuid === show.uuid ? show : s)),
+    })),
+  removeShow: (show) =>
+    set((state) => ({
+      ...state,
+      shows: state.shows?.filter((s) => s.uuid !== show.uuid),
+    })),
+  updateSubject: (subject) =>
+    set((state) => ({
+      ...state,
+      subject,
+    })),
+}));
+
+const { addShow, updateShow, removeShow, updateSubject } = useStore.getState();
+
+const Home = () => {
+  updateSubject("Queens Hall Presents: 2021");
   return (
     <>
       <Head>
@@ -31,25 +62,48 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex h-screen w-screen flex-row">
-        <SideBar emailJSON={emailJSON} setEmailJSON={setEmailJSON} />
+        <SideBar />
         <main className="flex min-h-screen flex-col"></main>
       </div>
     </>
   );
-}
+};
 
-const SideBar: React.FC<{
-  emailJSON: Email;
-  setEmailJSON: (newEmailJSON: Email) => void;
-}> = ({ emailJSON, setEmailJSON }) => {
+export default Home;
+
+const SideBar: React.FC = () => {
   return (
     <div className="flex h-full w-1/4 flex-col gap-2 bg-stiletto-700 px-8 py-4">
-      <Image src={qhLogo} alt="Queens Hall logo" height={110} />
+      <Image src={qhLogo} alt="Queens Hall logo" height={110} priority />
       <span className="mt-2 border-b-2 border-white" />
-      <div className="flex h-full w-full flex-col gap-2 overflow-y-auto">
-        <ShowEditor />
-        <AddIcon emailJSON={emailJSON} setEmailJSON={setEmailJSON} />
+      <input
+        className="border-white bg-transparent text-xl font-bold tracking-tight text-white focus:border-b-2 focus:outline-none"
+        placeholder="Email Subject"
+      />
+      <div className="scrollbar-hide flex h-full w-full flex-col gap-2 overflow-y-auto overflow-x-clip">
+        {useStore(
+          (state) =>
+            state.shows?.map((show) => (
+              <ShowEditor key={show.uuid} show={show} />
+            )),
+        )}
+        <span className="flex justify-center">
+          <AddIcon
+            onClick={() =>
+              addShow({
+                uuid: v4(),
+                date: "",
+                title: "",
+                description: "",
+                ticketLink: "",
+              })
+            }
+          />
+        </span>
       </div>
+      <button className="rounded bg-white px-4 py-2 font-bold text-stiletto-700">
+        Send Email
+      </button>
       <h4 className="text-center text-xs text-white">
         © William Stoneham 2023
       </h4>
@@ -57,39 +111,49 @@ const SideBar: React.FC<{
   );
 };
 
-const AddIcon: React.FC<{
-  emailJSON: Email;
-  setEmailJSON: (newEmailJSON: Email) => void;
-}> = (emailJSON, setEmailJSON) => {
+const AddIcon: React.FC<{ onClick?: React.MouseEventHandler }> = ({
+  onClick,
+  ...props
+}) => {
   return (
-    <span className="relative flex justify-center">
-      <svg
-        className="slate-600 hover:slate-800 aspect-square w-10 cursor-pointer text-white"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-        ></path>
-      </svg>
-    </span>
+    <svg
+      className="slate-600 hover:slate-800 aspect-square w-10 cursor-pointer text-white"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      onClick={onClick}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      ></path>
+    </svg>
   );
 };
 
-const ShowEditor = () => {
+const ShowEditor: React.FC<{ show: Show }> = ({ show, ...props }) => {
   return (
-    <div className="h-max w-full flex-row gap-2 border-b-2 border-white text-white">
+    <div
+      className="h-max w-full flex-row gap-2 border-b-2 border-white text-white"
+      {...props}
+    >
       <span className="my-2 flex justify-between">
         {/* <input className="flex-1 cursor-text bg-transparent font-semibold focus:bg-transparent focus:outline-none" /> */}
         <input
           className="w-10 flex-1 cursor-text border-white bg-transparent text-xl font-extrabold text-white hover:text-gray-200 focus:bg-transparent focus:outline-none"
           placeholder="example show"
+          value={show.title}
+          onChange={(e) => {
+            updateShow({ ...show, title: e.target.value });
+          }}
         />
-        <span className="aspect-square h-5 cursor-pointer">
+        <span
+          className="aspect-square h-5 cursor-pointer"
+          onClick={(e) => removeShow({ ...show })}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -110,6 +174,10 @@ const ShowEditor = () => {
         <input
           type="date"
           className="flex-1 cursor-text border-white bg-transparent focus:border-b-2 focus:outline-none"
+          value={show.date}
+          onChange={(e) => {
+            updateShow({ ...show, date: e.target.value });
+          }}
         />
       </span>
       <span className="my-2 flex gap-2">
@@ -118,12 +186,20 @@ const ShowEditor = () => {
           type="url"
           className="flex-1 cursor-text border-white bg-transparent focus:border-b-2 focus:outline-none"
           placeholder="https://example.com"
+          value={show.ticketLink}
+          onChange={(e) => {
+            updateShow({ ...show, ticketLink: e.target.value });
+          }}
         />
       </span>
       <span>
         <textarea
           className="w-full bg-transparent focus:outline-none"
           placeholder="description"
+          value={show.description}
+          onChange={(e) => {
+            updateShow({ ...show, description: e.target.value });
+          }}
         />
       </span>
       <input type="file" className="hidden" />
